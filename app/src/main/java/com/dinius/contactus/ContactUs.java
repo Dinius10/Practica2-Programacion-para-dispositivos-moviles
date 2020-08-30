82fa77779a0544db9ac9fd9213a262b68c81f0b5
Dinius10 <70424265+Dinius10@users.noreply.github.com>
2020-08-29 23:54:12 -0400

    Add files via upload

---
+++ app/src/main/java/com/dinius/contactus/ContactUs.java
@@ -0,0 +1,14 @@
+package com.dinius.contactus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us);
+    }
+}
